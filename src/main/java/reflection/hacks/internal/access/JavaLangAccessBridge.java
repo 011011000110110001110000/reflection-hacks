9e42d4fb257164d9962fbf557bf244c40689fc47
011011000110110001110000 <106524046+011011000110110001110000@users.noreply.github.com>
2023-12-13 21:30:36 +0100

    Add @link tag for jdk.internal.access package reference in the javadoc for JavaLangAccessBridge, replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addExports(Module, String, Module), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addExportsToAllUnnamed(Module, String), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addExports(Module, String), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addOpens(Module, String, Module), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addOpensToAllUnnamed(Module, String, Module), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addOpens(Module, String), replace Handles#invoke(MethodHandle, Object...) invocation with explicit try-catch block in JavaLangAccessBridge#addEnableNativeAccess(Module), switch from Proxy#newProxyInstance(ClassLoader, Class[], InvocationHandler) to Proxy#getProxyClass(ClasLoader, Class...) in JavaLangAccessBridge#gainInternalAccess(), add explanation of how JavaLangAccessBridge#gainInternalAccess() can break encapsulation in its javadoc

--- src/main/java/reflection/hacks/internal/access/JavaLangAccessBridge.java
+++ src/main/java/reflection/hacks/internal/access/JavaLangAccessBridge.java
@@ -1,7 +1,6 @@
 package reflection.hacks.internal.access;
 
 import org.jetbrains.annotations.NotNull;
-import reflection.hacks.api.invoke.Handles;
 import reflection.hacks.api.reflect.Classes;
 import reflection.hacks.api.reflect.Reflection;
 import reflection.hacks.internal.injector.Injector;
@@ -14,7 +13,7 @@ import java.lang.reflect.Proxy;
 
 /**
  * Helper class that serves as a bridge between {@link Reflection} (and its internals) and {@link jdk.internal.access.JavaLangAccess}. <br>
- * The purpose of this class is to get rid of the need to export the "jdk.internal.access" package to this class' module via the {@code --add-exports}
+ * The purpose of this class is to get rid of the need to export the {@link jdk.internal.access} package to this class' module via the {@code --add-exports}
  * argument at compile / run time.
  *
  * @author <a href=https://github.com/011011000110110001110000>011011000110110001110000</a>
@@ -161,7 +160,11 @@ public final class JavaLangAccessBridge {
      * @param m2  The module the package should be exported to
      */
     public static void addExports(final @NotNull Module m1, final @NotNull String pkg, final @NotNull Module m2) {
-        Handles.invoke(JavaLangAccessBridge.ADD_EXPORTS_TO_MODULE_MH, m1, pkg, m2);
+        try {
+            JavaLangAccessBridge.ADD_EXPORTS_TO_MODULE_MH.invokeExact(m1, pkg, m2);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
@@ -173,7 +176,11 @@ public final class JavaLangAccessBridge {
      * @param pkg The name of the package to export
      */
     public static void addExportsToAllUnnamed(final @NotNull Module m, final @NotNull String pkg) {
-        Handles.invoke(JavaLangAccessBridge.ADD_EXPORTS_TO_ALL_UNNAMED_MODULES_MH, m, pkg);
+        try {
+            JavaLangAccessBridge.ADD_EXPORTS_TO_ALL_UNNAMED_MODULES_MH.invokeExact(m, pkg);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
@@ -185,7 +192,11 @@ public final class JavaLangAccessBridge {
      * @param pkg The name of the package to export
      */
     public static void addExports(final @NotNull Module m, final @NotNull String pkg) {
-        Handles.invoke(JavaLangAccessBridge.ADD_EXPORTS_TO_ALL_MODULES_MH, m, pkg);
+        try {
+            JavaLangAccessBridge.ADD_EXPORTS_TO_ALL_MODULES_MH.invokeExact(m, pkg);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
@@ -198,7 +209,11 @@ public final class JavaLangAccessBridge {
      * @param m2  The module to open the package to
      */
     public static void addOpens(final @NotNull Module m1, final @NotNull String pkg, final @NotNull Module m2) {
-        Handles.invoke(JavaLangAccessBridge.ADD_OPENS_TO_MODULE_MH, m1, pkg, m2);
+        try {
+            JavaLangAccessBridge.ADD_OPENS_TO_MODULE_MH.invokeExact(m1, pkg, m2);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
@@ -210,7 +225,11 @@ public final class JavaLangAccessBridge {
      * @param pkg The name of the package to open
      */
     public static void addOpensToAllUnnamed(final @NotNull Module m, final @NotNull String pkg) {
-        Handles.invoke(JavaLangAccessBridge.ADD_OPENS_TO_ALL_UNNAMED_MODULES_MH, m, pkg);
+        try {
+            JavaLangAccessBridge.ADD_OPENS_TO_ALL_UNNAMED_MODULES_MH.invokeExact(m, pkg);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
@@ -221,36 +240,50 @@ public final class JavaLangAccessBridge {
      * @param m The module to update
      */
     public static void addEnableNativeAccess(final @NotNull Module m) {
-        Handles.invoke(JavaLangAccessBridge.ADD_ENABLE_NATIVE_ACCESS_TO_MODULE_MH, m);
+        try {
+            JavaLangAccessBridge.ADD_ENABLE_NATIVE_ACCESS_TO_MODULE_MH.invokeExact(m);
+        } catch (final Throwable t) {
+            throw Throwables.sneakyThrow(t);
+        }
     }
 
     /**
      * Exploits the Proxy API to gain access to the {@link jdk.internal.access} package,
      * which is normally not visible to modules that are not part of the JDK implementation.
      *
-     * @implNote TODO: add detailed explanation of how this works
+     * @implNote This method exploits the fact that a proxy class must be able to access the interfaces that it implements.
+     * More specifically, we generate a proxy class for the {@link jdk.internal.access.JavaLangAccess} interface, which is public
+     * but in a non-exported package inside the {@code java.base} module via {@link java.lang.reflect.Proxy#getProxyClass(ClassLoader, Class...)}.
+     * To avoid access errors, the generated proxy class is put in a dynamic module, which is a runtime-generated named module that reads {@link jdk.internal.access},
+     * and the {@code java.base} module is updated to export {@link jdk.internal.access} to the dynamic module.
+     * For more information on dynamic modules, see the <a href="https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/reflect/Proxy.html#dynamicmodule"> Dynamic Modules section</a>
+     * in the documentation for {@link java.lang.reflect.Proxy}.
+     * <p>
+     * Once the dynamic module is defined to {@link reflection.hacks.internal.injector.Injector.Loader our ClassLoader}, we can retrieve the package
+     * that {@link jdk.internal.access} was exported to by invoking {@link java.lang.Class#getPackageName()} on the generated proxy class.
+     * Once we know the package name, we can {@link reflection.hacks.internal.injector.Injector#generateIn(String) generate a class in the same package}
+     * and {@link reflection.hacks.internal.injector.Injector.Loader#defineAndLoad(byte[]) define it to our ClassLoader}.
+     * The generated class will have access to the {@link jdk.internal.access.SharedSecrets} class, which allows us to obtain an instance of {@link jdk.internal.access.JavaLangAccess}.
+     * At this point we can just obtain {@link java.lang.invoke.MethodHandle MethodHandle}s for all the methods that we need and {@link java.lang.invoke.MethodHandle#bindTo(Object) bind them}
+     * to the instance so they can be invoked by methods of this class as if they were handles for static methods.
+     * </p>
      */
+    @SuppressWarnings("deprecation")
     private static void gainInternalAccess() {
         final String javaLangAccessName = "jdk.internal.access.JavaLangAccess";
         final Injector.Loader injectorLoader = new Injector.Loader();
         final Class<?> javaLangAccessInterface;
-        final Class<?>[] interfaces;
 
         try {
             // noinspection Java9ReflectionClassVisibility
             javaLangAccessInterface = Class.forName(javaLangAccessName);
-            interfaces = new Class[]{
-                    javaLangAccessInterface
-            };
 
-            // noinspection SuspiciousInvocationHandlerImplementation
-            final Object proxyInstance = Proxy.newProxyInstance(
+            final Class<?> proxyClass = Proxy.getProxyClass(
                     injectorLoader,
-                    interfaces,
-                    (proxy, method, arguments) -> null
+                    javaLangAccessInterface
             );
 
-            final String packageName = proxyInstance.getClass().getPackageName().replace(".", "/");
+            final String packageName = proxyClass.getPackageName().replace(".", "/");
 
             injectorLoader.defineAndLoad(Injector.generateIn(packageName));
 
