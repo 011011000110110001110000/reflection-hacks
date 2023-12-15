5f973325781632a19552cdf432a3fcfbcd925957
011011000110110001110000 <106524046+011011000110110001110000@users.noreply.github.com>
2023-12-15 21:48:27 +0100

    Fix broken links in reflection/hacks/api/package-info.java

--- src/main/java/reflection/hacks/api/package-info.java
+++ src/main/java/reflection/hacks/api/package-info.java
@@ -1,8 +1,8 @@
 /**
  * This package contains the public facing API for the {@code reflection-hacks} library.
  * <p>
- * The API is split in two main packages: {@link me.lollopollqo.reflection.hacks.api.reflect} and
- * {@link me.lollopollqo.reflection.hacks.api.invoke}, which provide unrestricted and easier to use versions of the
+ * The API is split in two main packages: {@link reflection.hacks.api.reflect} and
+ * {@link reflection.hacks.api.invoke}, which provide unrestricted and easier to use versions of the
  * {@linkplain java.lang.reflect reflection} and {@linkplain java.lang.invoke invocation} APIs respectively.
  * Moreover, none of the methods provided by the API explicitly throw any checked exceptions, unlike the
  * core reflection and invocation APIs.
